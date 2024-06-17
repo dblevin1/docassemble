c1da008d04075e817d5faabd3a61579ded647371
Daniel Blevins <daniel.blevins@asu.edu>
2024-06-17 11:13:33 -0700

    add docker-compose and push local script

--- push_locally.sh
+++ push_locally.sh
@@ -1,1 +1,14 @@
-../docker-docassemble/my-docassemble/push_locally.sh
+#!/usr/bin/bash
+
+PACKAGE_DIR="$(cd "$(dirname "$0")" && pwd)"
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")" # This follows the symlink to the docker-compose file
+echo "PACKAGE_DIR=$PACKAGE_DIR"
+echo "SCRIPT_DIR=$SCRIPT_DIR"
+cd $SCRIPT_DIR
+
+docker-compose cp "$PACKAGE_DIR/docassemble_webapp/docassemble/webapp/." docassemble:/usr/share/docassemble/local3.10/lib/python3.10/site-packages/docassemble/webapp/
+docker-compose cp "$PACKAGE_DIR/docassemble_base/docassemble/base/." docassemble:/usr/share/docassemble/local3.10/lib/python3.10/site-packages/docassemble/base/
+docker-compose exec docassemble chown -R www-data:www-data /usr/share/docassemble/local3.10/lib/python3.10/site-packages/docassemble/webapp/
+docker-compose exec docassemble chown -R www-data:www-data /usr/share/docassemble/local3.10/lib/python3.10/site-packages/docassemble/base/
+docker-compose exec docassemble supervisorctl start reset
+exit
